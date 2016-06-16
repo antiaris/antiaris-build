ea7af30ba5a0883475f5f826c1a21bb02db59587
yinyong <yanni4night@gmail.com>
2016-06-17 00:28:38 +0800

    Compile node_modules to static

--- index_sieve.js
+++ index_sieve.js
@@ -11,9 +11,12 @@
  */
 'use strict';
 
+const path = require('path');
 const rimraf = require('rimraf');
 const less = require('less');
 const mkdirp = require('mkdirp');
+const nodeResolve = require('antiaris-node-resolve');
+const c2s = require('antiaris-transform-commonjs-modules-systemjs');
 const sieve = require('./lib/sieve');
 const {
     filestamp
@@ -42,6 +45,18 @@ const resourceMap = {};
 
 rimraf.sync(OUTPUT); // TODO:on demand
 
+
+// TODO:Replace variables
+sieve.hook(`**/*.*`, ({
+    file,
+    content
+}) => {
+    return Promise.resolve({
+        file,
+        content
+    });
+});
+
 // Binary files
 sieve.hook(`{${SRC},${NODE_MODULES}}/**/*.{${BINARY_RESOURCE}}`, ({
     file,
@@ -67,6 +82,7 @@ sieve.hook(`{${SRC},${NODE_MODULES}}/**/*.{${BINARY_RESOURCE}}`, ({
     });
 });
 
+// Less files
 sieve.hook(`{${SRC},${NODE_MODULES}}/**/*.less`, ({
     file,
     content
@@ -103,6 +119,63 @@ sieve.hook(`{${SRC},${NODE_MODULES}}/**/*.less`, ({
     });
 });
 
+// "node_modules" Script files
+// 这里的文件需要生成两份代码，同构之用
+// (1)[Server]原文不动复制过去
+// (2)[Client]生成SystemJS风格，计算时间戳
+sieve.hook(`${NODE_MODULES}/**/*.js`, ({
+    file,
+    content
+}) => {
+    return new Promise((resolve, reject) => {
+        const moduleId = `${NAMESPACE}:${file}`;
+
+        c2s.transformFile(L(file), {
+            moduleId,
+            translateDep: dep => {
+                let p = nodeResolve.resolve(L(file), dep);
+                if (!p) {
+                    warn(`Dependency "${dep}" not found for ${file}`);
+                    return p;
+                }
+                return `${NAMESPACE}:` + path.relative(CWD, p);
+            }
+        }, (err, result) => {
+            if (err) {
+                reject(err);
+            } else {
+                let {
+                    filename
+                } = filestamp.sync(L(file));
+
+                resourceMap[moduleId] = {
+                    uri: `${NAMESPACE}/${filename}`,
+                    deps: result.deps
+                };
+                resolve({
+                    file: `../static/${NAMESPACE}/${filename}`,
+                    content: result.code
+                });
+            }
+        });
+    });
+});
+
+// "src" Script files
+// 这里的文件需要生成两份代码，同构之用
+// (1)[Server]Babel转换成ES5/CommonJS
+// (2)[Client]Babel转换成ES5/CommonJS，生成SystemJS风格，计算时间戳
+sieve.hook(`${SRC}/**/*.{js,jsx}`, ({
+    file,
+    content
+}) => {
+    return Promise.resolve({
+        file,
+        content
+    });
+});
+
+// Final Build
 sieve.build(CWD).then(() => {
     return W(`${OUTPUT}/resource-map.json`, JSON.stringify(resourceMap, null, 4));
 }).catch(e => {
