244f8b5ebd5c530c82af44b05b48422c157a8e80
yinyong <yanni4night@gmail.com>
2016-06-19 21:01:55 +0800

    single

--- test/test.js
+++ test/test.js
@@ -12,11 +12,47 @@
 'use strict';
 
 const assert = require('assert');
+const extend = require('lodash/extend');
+const Transformer = require('../lib/transformer/transformer');
+
+class TestTransformer extends Transformer {
+    _transform(seed) {
+        const {
+            content
+        } = seed;
+        return new Promise(resolve => {
+            resolve(extend({}, seed, {
+                content: content + content
+            }));
+        });
+    }
+}
 
 describe('antiaris-build', () => {
     describe('transformer', () => {
-        it('#test', done => {
-            done();
+        it('#should make content transformed', done => {
+            const t = new TestTransformer();
+            t.transform({
+                filename: 'a.js',
+                content: 'a'
+            }).then(seed => {
+                assert.deepEqual(seed.content, 'aa');
+                done();
+            }).catch(e => {
+                console.error(e)
+            });
+        });
+        it('should skip if content is null', done => {
+            const t = new TestTransformer();
+            t.transform({
+                filename: 'a.js',
+                content: null
+            }).then(seed => {
+                assert.deepEqual(seed.content, null);
+                done();
+            }).catch(e => {
+                console.error(e)
+            });
         });
     });
 });
